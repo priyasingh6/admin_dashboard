bcd37963307626ad69bc4c543a19d7883944b2b3
priya singh <ps9986219@gmail.com>
2025-08-08 02:25:39 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,20 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='siteProject' element={<SiteProject />} />
-          <Route path='enquiry' element={<div>Enquiry</div>} />
+          <Route path='enquiry' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl' >Enquiry</div>} />
           <Route path='site-project' element={<SiteProject />} />
-          <Route path='influencer-network' element={<div>Influencer Network</div>} />
-          <Route path='customer-network' element={<div>Customer Network</div>} />
-          <Route path='orders' element={<div>Orders</div>} />
-          <Route path='accounts' element={<div>Accounts</div>} />
-          <Route path='schemes-incentive' element={<div>Schemes/Incentive</div>} />
-          <Route path='secondary-billing-upload' element={<div>Secondary Billing Upload</div>} />
-          <Route path='dispatch' element={<div>Dispatch</div>} />
-          <Route path='master-box' element={<div>Master Box</div>} />
-          <Route path='stock' element={<div>Stock</div>} />
-          <Route path='attendance' element={<div>Attendance</div>} />
-          <Route path='checkin' element={<div>Checkin</div>} />
-          <Route path='leave' element={<div>Leave</div>} />
+          <Route path='influencer-network' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl' >Influencer Network</div>} />
+          <Route path='customer-network' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Customer Network</div>} />
+          <Route path='orders' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Orders</div>} />
+          <Route path='accounts' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Accounts</div>} />
+          <Route path='schemes-incentive' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Schemes/Incentive</div>} />
+          <Route path='secondary-billing-upload' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Secondary Billing Upload</div>} />
+          <Route path='dispatch' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Dispatch</div>} />
+          <Route path='master-box' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Master Box</div>} />
+          <Route path='stock' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Stock</div>} />
+          <Route path='attendance' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Attendance</div>} />
+          <Route path='checkin' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Checkin</div>} />
+          <Route path='leave' element={<div className='flex h-screen dark:bg-gray-900 dark:text-white p-6 text-xl'>Leave</div>} />
         </Route>
       </>
     )
